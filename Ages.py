5d20e3ee64f25406beb43a5772e2440ddb66f95a
timruet <tim.ruettinger@online.de>
2019-06-13 11:57:05 +0200

    Update Ages.py

--- Ages.py
+++ Ages.py
@@ -1,3 +1,22 @@
+#faster
+import datetime as dt
+ages = pd.Series()
+tweets_df["createdAt"] = pd.to_datetime(tweets_df["createdAt"])
+for j in range(len(users_df["id"]):   
+    try:
+        k = float(users_df["id"][j])
+        x = tweets_df["createdAt"].where(tweets_df["userID"]==k).max()
+        y = tweets_df["createdAt"].where(tweets_df["userID"]==k).min()
+        a = x-y
+        a = a.total_seconds()
+        ages = ages.append(to_append=pd.Series([a],index=[k]))        
+    except: 
+        pass
+
+
+
+
+
 #Panda Series ages contains the ages with the userID as corresponding index
 import datetime as dt
 mf = pd.DataFrame(columns = ["age"])
