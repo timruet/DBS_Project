31628e8a0a5d70cd26a631a96ef23dd9f4bf54f6
timruet <tim.ruettinger@online.de>
2019-06-13 12:11:57 +0200

    Update NumberofFollowers.py

--- NumberofFollowers.py
+++ NumberofFollowers.py
@@ -1,3 +1,12 @@
+#faster
+nf = pd.Series()
+k= 0
+for i in range(len(users_df["id"])): 
+    k = followings_df["userID"][0]
+    x = followings_df["followerID"].where(followings_df["userID"]==k)
+    x = x.dropna()
+    nf=nf.append(pd.Series([len(x)],index=[k]))
+
 #gives dataframe with userIDs as columns and followers in rows
 l = pd.Series()
 nf = pd.DataFrame()
