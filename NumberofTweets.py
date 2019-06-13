ad7c26b0cbe0b2c572c27dd9cae581e8ce758123
timruet <tim.ruettinger@online.de>
2019-06-13 12:41:41 +0200

    Update NumberofTweets.py

--- NumberofTweets.py
+++ NumberofTweets.py
@@ -1,3 +1,16 @@
+#faster
+#number of tweets
+nf = pd.Series()
+k=0
+for i in range(len(users_df["id"])): 
+    try:
+        k = float(users_df["id"][i])
+        x = tweets_df["tweetID"].where(tweets_df["userID"]==k)
+        x = x.dropna()
+        nf=nf.append(pd.Series([len(x)],index=[k]))
+    except:
+        pass
+
 #Pandas Series with number of tweets with corresponding userID as index
 l = pd.Series()
 nt = pd.Series()
