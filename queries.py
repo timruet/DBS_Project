6e767b071197a6b792e80d66f4bf3bbce9fdd25c
timruet <tim.ruettinger@online.de>
2019-06-27 09:20:47 +0200

    Update queries.py

--- queries.py
+++ queries.py
@@ -3,7 +3,6 @@ import psycopg2
 def connect():
     conn = None
     try:
-        # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(host="localhost", database="dbs", user="postgres", password="postgres")
 
@@ -89,7 +88,6 @@ def connect():
         print("Number of users who have no fans and no romantic relationship:" + " " + result)
 
 
-        # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
