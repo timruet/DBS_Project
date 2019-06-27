17b6de30bf93230c18f1727d9ec0dd0306482af1
timruet <tim.ruettinger@online.de>
2019-06-27 09:19:41 +0200

    Update NumberofFollowers.py

--- NumberofFollowers.py
+++ NumberofFollowers.py
@@ -11,16 +11,3 @@ for i in range(len(users_df["id"])):
         ifo[users_df["id"][i]]=x
     except:
         pass
-
-#gives dataframe with userIDs as columns and followers in rows
-l = pd.Series()
-nf = pd.DataFrame()
-k= 0
-for i in range(len(followings_df["userID"])):
-    if not(followings_df["userID"][i] in l.unique()):
-        l = l.append(to_append=pd.Series([followings_df["userID"][i]]))
-        l_i = pd.Series() 
-        for j in (range(len(followings_df["userID"]))):
-            if followings_df["userID"][i]==followings_df["userID"][j]:
-                l_i = l_i.append(to_append=pd.Series([followings_df["followerID"][j]]),ignore_index=True)
-        nf[str(followings_df["userID"][i])]= l_i
