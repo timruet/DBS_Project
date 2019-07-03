f216778f74b19d59d3b7d79a5e4316b5779d9076
timruet <tim.ruettinger@online.de>
2019-07-03 14:49:35 +0200

    Create 1q.py

---
+++ 1q.py
@@ -0,0 +1,19 @@
+def connect():
+    conn = None
+    try:
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="12345")
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT MAX(age) FROM mfb_user")
+
+        result = cur.fetchone()[0]
+        print("Age of the oldest person:" + " " + str(result))
+    cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
