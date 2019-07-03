e5c7d623aeae32d15e08c93ee36555ad8d7d3a7a
timruet <tim.ruettinger@online.de>
2019-07-03 14:51:26 +0200

    Create 6q.py

---
+++ 6q.py
@@ -0,0 +1,31 @@
+def connect():
+    conn = None
+    try:
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="12345")
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT
+        DISTINCT
+        userid
+        FROM
+        User
+        EXCEPT
+        SELECT
+        userid
+        FROM
+        isfanof
+        NATURAL
+        JOIN
+        relationship)")
+
+        result = cur.fetchone()[0]
+        print("Number of users who have no fans and no romantic relationship:" + " " + str(result))
+    cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
