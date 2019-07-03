6907748c37cc31b0c9132e3adf4b3539928b1dcc
tugot17 <piotr635@gmail.com>
2019-07-03 22:04:47 +0200

    Queries added

--- miniFBFlask/app.py
+++ miniFBFlask/app.py
@@ -1,21 +1,64 @@
 from flask import Flask, render_template, request
-from jinja2 import Template
+from queries import get_user_data_based_on_user_id, \
+    get_user_fans_by_user_id, \
+    get_who_user_dates_by_user_id, get_who_user_marries_by_user_id
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def mini_fb():
+@app.route('/<user_id>')
+def show_user(user_id=614754689):
     get_user_data()
-    return render_template("index.html")
+
+    # ID = 1
+    #
+    # age = 2
+    #
+    # income = 3
+
+    # fans = ["fan1", "fan2", "fan3"]
+
+    dates = ["date1", "date2", "date3"]
+
+    marriages = ["marriage1", "marriage2", "marriage3"]
+
+    user_query_result = get_user_data_based_on_user_id(user_id)
+
+    ID = user_query_result[0]
+
+    age = user_query_result[3]
+
+    income = user_query_result[4]
+
+    name = user_query_result[2]
+
+    fan_query_result = get_user_fans_by_user_id(user_id)
+
+    dates_query_result = get_who_user_dates_by_user_id(user_id)
+
+    marriage_query_result = get_who_user_marries_by_user_id(user_id)
+
+
+
+    return render_template("index.html", name = name ,ID = ID,
+                           age=age, income=income, fans=fan_query_result,
+                           dates=dates_query_result, marriages=marriage_query_result)
+
+
+
+@app.route("/search")
+@app.route("/search/<id>")
+def search(id="2137"):
+    return render_template("search.html", screenName = id)
 
 def get_user_data():
     name = request.args.get('name')
-    
+
     id = request.args.get('ID')
     age = request.args.get('age')
     income = request.args.get('income')
     print(name, id, age, income)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- miniFBFlask/queries.py
+++ miniFBFlask/queries.py
@@ -1,39 +1,23 @@
 import psycopg2
 
 
-def get_oldest():
-    conn = None
-    try:
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres")
 
-        cur = conn.cursor()
-
-        cur.execute("SELECT MAX(age) FROM facebook_user")
-
-        result = cur.fetchone()[0]
-        cur.close()
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-    return result
-
+DATABASE = "postgres"
+USER     = "postgres"
+PASSWORD = "postgres"
 
 def get_user_data_based_on_user_id(user_id):
     conn = None
     result = None
 
     try:
-        conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="postgres")
+        conn = psycopg2.connect(host="localhost", database=DATABASE, user=USER, password=PASSWORD)
 
         cur = conn.cursor()
 
-        cur.execute(f"SELECT id, screenname, name FROM facebook_user where id = {user_id}")
+        cur.execute(f"SELECT id, screenname, name, age, income "
+                    f"FROM facebook_user "
+                    f"where id = {user_id}")
 
         result = cur.fetchone()
         cur.close()
@@ -46,5 +30,105 @@ def get_user_data_based_on_user_id(user_id):
     return result
 
 
+def get_number_of_records():
+    conn = None
+    result = None
 
-print(get_user_data_based_on_user_id(483674751))
+    try:
+        conn = psycopg2.connect(host="localhost", database=DATABASE, user=USER, password=PASSWORD)
+
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT COUNT(id) FROM facebook_user")
+
+        result = cur.fetchone()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return result
+
+
+def get_user_fans_by_user_id(user_id):
+    conn = None
+    result = None
+
+    try:
+        conn = psycopg2.connect(host="localhost", database=DATABASE, user=USER, password=PASSWORD)
+
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT DISTINCT facebook_user.screenname "
+                    f"FROM facebook_user "
+                    f"INNER JOIN is_fan_of "
+                    f"ON facebook_user.id = is_fan_of.fanid "
+                    f"WHERE is_fan_of.idolid = {user_id}")
+
+        result = cur.fetchall()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return result
+
+def get_who_user_dates_by_user_id(user_id):
+    conn = None
+    result = None
+
+    try:
+        conn = psycopg2.connect(host="localhost", database=DATABASE, user=USER, password=PASSWORD)
+
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT DISTINCT facebook_user.screenname "
+                    f"FROM facebook_user "
+                    f"INNER JOIN dates ON facebook_user.id = dates.userid_2 "
+                    f"WHERE dates.userid_1 = {user_id}")
+
+        result = cur.fetchall()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return result
+
+
+
+
+def get_who_user_marries_by_user_id(user_id):
+    conn = None
+    result = None
+
+    try:
+        conn = psycopg2.connect(host="localhost", database=DATABASE, user=USER, password=PASSWORD)
+
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT DISTINCT facebook_user.screenname "
+                    f"FROM facebook_user "
+                    f"INNER JOIN is_married_to imt on facebook_user.id = imt.userid_2 "
+                    f"WHERE imt.userid_1 = {user_id}")
+
+        result = cur.fetchall()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return result
+
+
+
+if __name__ == "__main__":
+    print(get_who_user_dates_by_user_id(1031571313352224768))
